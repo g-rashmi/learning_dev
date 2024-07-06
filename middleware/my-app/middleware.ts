651ec4bb5ca3ad49651365be3f0618bf26e4a99d
g-rashmi <rashmi.gupta7018@gmail.com>
2024-07-06 15:39:27 +0530

    learn middleware in next

---
+++ middleware/my-app/middleware.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+let requestCount = 0;
+export function middleware(request: NextRequest) {
+  requestCount++;
+  console.log("njjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjumber of requestsppppppppppppp is " + requestCount);
+  return  NextResponse.next()
+}
