110621708b6a32a7afe10b6157969bdb8636494d
g-rashmi <rashmi.gupta7018@gmail.com>
2024-07-14 19:51:44 +0530

    express-rate-limit

--- rate_limit/backend/src/index.ts
+++ rate_limit/backend/src/index.ts
@@ -2,7 +2,18 @@ import express from 'express';
 
 const app = express();
 const PORT = 3000;
+import { rateLimit } from 'express-rate-limit'
 
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
 app.use(express.json());
 
 // Store OTPs in a simple in-memory object
