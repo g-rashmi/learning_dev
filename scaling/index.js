7bd661b5ff9b998660969126bf3a21928193bf5b
g-rashmi <rashmi.gupta7018@gmail.com>
2024-07-31 18:59:33 +0530

    learning

--- scaling/index.js
+++ scaling/index.js
@@ -5,8 +5,42 @@ const totalcpu=os.cpus().length
 
 const port=3000;
 const app=express()
- 
-app.listen(port, () => {
-  console.log(totalcpu)
-  console.log(`App listening on port ${port}`);
-});
+if (cluster.isPrimary) {
+  console.log(`Number of CPUs is ${totalcpu}`);
+  console.log(`Primary ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < totalcpu; i++) {
+    cluster.fork();
+  } 
+
+  cluster.on("exit",(worker,code,signal)=>{
+    console.log(`worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
+  })
+}else {
+  const app = express();
+  console.log(`Worker ${process.pid} started`);
+
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+
+  app.get("/api/:n", function (req, res) {
+    let n = parseInt(req.params.n);
+    let count = 0;
+
+    if (n > 5000000000) n = 5000000000;
+
+    for (let i = 0; i <= n; i++) {
+      count += i;
+    }
+
+    res.send(`Final count is ${count} ${process.pid}`);
+  });
+
+  app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+  });
+}
